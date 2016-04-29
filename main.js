607e3e3b64f1b114a503454c0e8c6ec81da9a5db
Alvaro Castillo <alvaro.castillo23@gmail.com>
2016-04-29 11:00:40 -0400

    Display artist name for the track searched

--- main.js
+++ main.js
@@ -1,9 +1,28 @@
 $(document).on("ready", function(){
+	$(".js-submit").on("submit", function(event){
+		event.preventDefault();
 
+		var theTrack = $("#track-name").val();
+		console.log(theTrack)
 
+		$.ajax({
+			url: "https://api.spotify.com/v1/search?type=track&query=" + theTrack,
+			success: function(theData){
+				console.log("It worked");
+				console.log(theData)
+				displayTitle(theData.tracks.items[0])
+			},
+			error: function(theError){
+				console.log("It failed");
+				console.log(theError.responseJSON);
+			}
 
+		})
 
-
-
-
+	})
 });
+
+function displayTitle(the_track){
+	var html = `${the_track.name}`
+	$(".js-title").text(html);
+}
