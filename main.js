13f6993c036738ff7545368e779457794da9bee3
Alvaro Castillo <alvaro.castillo23@gmail.com>
2016-04-29 18:50:03 -0400

    Display modal with information about the artist when clicked on the name

--- main.js
+++ main.js
@@ -28,6 +28,30 @@ $(document).on("ready", function(){
 		$('.js-play-audio').on('timeupdate', printTime);
 	})
 
+	$(".js-artist").on("click", function(){
+		console.log("THE MODAL PLEASE")
+		var artistName = $(".js-artist").val();
+		console.log(artistName)
+
+		$.ajax({
+			url: "https://api.spotify.com/v1/search?type=artist&query=" + artistName,
+			success: function(theData){
+				console.log("Retrieving data about the artist")
+				console.log(theData);
+				showInfoinModal(theData.artists.items);
+
+
+
+
+				$('.js-modal').modal("show");
+			},
+			error: function(theError){
+				console.log("Failed retrieving data about the artist")
+				console.log(theError.responseJSON);
+			}
+		})
+	})
+
 });
 
 //------- function to display track title ------//
@@ -38,10 +62,15 @@ function displayTitle(the_items){
 	// $("#track-name").val("");
 }
 
+//------- function to display artist name ------//
+
 function displayArtist(the_items){
 	var html = `${the_items[0].artists[0].name}`
 	$(".js-artist").text(html);
+	$(".js-artist").val(html);
 }
+
+//------- function to display cover picture ------//
 
 function displayCover(the_items){
 	var html = `<img src="${the_items[0].album.images[0].url}">`
@@ -49,13 +78,15 @@ function displayCover(the_items){
 	$(".js-cover").append(html);
 }
 
-//--------- function to play track -------//
+//--------- function to get audio source -------//
 
 function getAudio(the_items){
 	var html = `<audio class= "js-play-audio" src="${the_items[0].preview_url}"></audio>`
 	$(".js-audio").empty();
 	$(".js-audio").append(html);
 }
+
+//------- function to play/pause audio ------//
 
 function playPauseTrack(the_items){
 	if ($(".js-play").hasClass('playing') === false){
@@ -69,6 +100,8 @@ function playPauseTrack(the_items){
 	}
 }
 
+//------- function to update progress bar ------//
+
 function printTime () {
   var current = $('.js-play-audio').prop('currentTime');
   console.debug('Current time: ' + current);
@@ -76,5 +109,35 @@ function printTime () {
 
   $('.seekbar').empty();
   $('.seekbar').append(html);
+}
+
+// ------- function to show the artist's info in modal---//
+
+function showInfoinModal(the_array){
+
+	$(".js-modal-name").empty();
+	$(".js-modal-followers").empty();
+	$(".js-modal-popularity").empty();
+	$(".js-modal-genres").empty();
+	$(".js-image").empty();
+
+
+	$(".js-modal-name").text(the_array[0].name);
+	$(".js-modal-followers").text(the_array[0].followers.total)
+	$(".js-modal-popularity").text(the_array[0].popularity);
+
+	var the_genres = the_array[0].genres
+
+	the_genres.forEach(function(the_genre){
+		var html = `
+			<li>
+				<p>${the_genre}</p>
+			</li>`
+		$(".js-modal-genres").append(html);
+	})
+
+	var the_image = `<img src="${the_array[0].images[0].url}" width="350px">`
+
+	$(".js-image").append(the_image);
 }
 
